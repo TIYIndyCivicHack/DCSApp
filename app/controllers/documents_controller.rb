450f9436f75f6943d45b2f1b2d05fe41bd8f9ae2
CosmicCandi <kalea.wolff@gmail.com>
2017-06-17 01:25:19 -0400

    Updates Document controller actions.

--- app/controllers/documents_controller.rb
+++ app/controllers/documents_controller.rb
@@ -1,22 +1,36 @@
 class DocumentsController < ApplicationController
 
   def index
+    @documents = @user.documents.all
+    render json: @documents
   end
 
   def create
+    @document = @user.documents.new(doc_params)
+      if @document.save
+        render json: @document.reload
+      else
+        render json: @document.errors.full_messages
+      end
   end
 
   def update
+    if @document.update!(doc_params)
+      render json: @document
+    else
+      render json: @document.errors.full_messages, status: 400
+    end
   end
 
   def destroy
+    @document.destroy
+    render json: ["Document deleted successfully."]
   end
 
   private
 
-  #TODO Add params to documents
-   def doc_params
-     params.permit()
+    def doc_params
+     params.permit(:type, :photo)
    end
 
 end
